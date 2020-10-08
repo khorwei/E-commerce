3f95a666776cc91391cc9de4758d208d86b9dcce
khorwei <51141868+khorwei@users.noreply.github.com>
2020-10-08 22:12:59 +0800

    Add files via upload

---
+++ sql/libros.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.9.2
+ */
+
+/**
+ * Database `libros`
+ */
+
+/* `libros`.`admins` */
+$admins = array(
+  array('admin_id' => '16','username' => 'coway','email' => 'coway@gmail.com','admin_password' => '610689e41a68011729a5f61aafd0b16b'),
+  array('admin_id' => '14','username' => 'admin3','email' => 'admin3@hotmail.com','admin_password' => '4b93c8e2e2ee6a09b9a4e75b0d619c4b'),
+  array('admin_id' => '15','username' => 'admin5','email' => 'admin5@hotmail.com','admin_password' => 'e10adc3949ba59abbe56e057f20f883e')
+);
+
+/* `libros`.`books` */
+$books = array(
+  array('book_id' => '22','bookname' => 'It\'s Kind of a Funny Story','author' => 'Ned Vizzini','price' => '20.50','category' => 'English Books','quantity' => '100','detail' => 'It\'s Kind of a Funny Story is a 2006 novel by American author Ned Vizzini. The book was inspired by Vizzini\'s own brief hospitalization for depression in November 2004. Ned Vizzini later died by suicide on December 19, 2013. The book received recognition as a 2007 Best Book for Young Adults from the American Library Association.
+A film adaptation, directed by Anna Boden and Ryan Fleck, was released in the United States on October 8, 2010.','image_path' => '../bookimage/Funny_Story_front.jpg','added_date' => '2020-07-02'),
+  array('book_id' => '23','bookname' => 'Nineteen Eighty-Four: A Novel','author' => 'George Orwell','price' => '25.00','category' => 'English Books','quantity' => '32','detail' => 'Nineteen Eighty-Four: A Novel, often published as 1984, is a dystopian novel by English novelist George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell\'s ninth and final book completed in his lifetime. Thematically, Nineteen Eighty-Four centres on the consequences of government over-reach, totalitarianism, mass surveillance, and repressive regimentation of all persons and behaviours within society. More broadly, it examines the role of truth and facts within politics and their manipulation.','image_path' => '../bookimage/1984first.jpg','added_date' => '2020-07-05'),
+  array('book_id' => '4','bookname' => 'FNAF: FAZBEAR FRIGHTS #02: FETCH','author' => 'Scott Cawthon','price' => '20.00','category' => 'English Books','quantity' => '9','detail' => 'The Fazbear Frights series continues with three more
+bone-chilling, novella-length tales to keep even the bravest Five
+Nights at Freddy\'s player up at night...
+After years of being kicked around, Greg, Alec and Oscar are ready
+to take control of their lives. Greg decides to put the controversial
+science he\'s been studying to the test. Alec launches a
+master plot to expose his golden sister for the spoiled brat he
+knows she is. And Oscar, ever the miniature grown-up his mom needs
+him to be, decides to take something he wants ... even though he
+knows it\'s wrong. But as these three will learn, control is a fragile
+thing in the sinister world of Five Nights at Freddy\'s.','image_path' => '../bookimage/FAZBEAR FRIGHTS.jpg','added_date' => '2020-06-30'),
+  array('book_id' => '5','bookname' => 'THINK LIKE A MONK : TRAIN YOUR MIND FOR PEACE AND PURPOSE EVERY DAY','author' => 'Jay Shetty','price' => '40.00','category' => 'English Books','quantity' => '15','detail' => 'Jay Shetty, social media superstar and host of the #1 podcast \'On Purpose\', distils the timeless wisdom he learned as a practising monk into practical steps anyone can take every day to live a less anxious, more meaningful life.
+Over the past three years, Jay Shetty has become a favourite in the hearts and minds of millions of people worldwide. One of his clips was the most watched video on Facebook last year, with over 360 million views. His social media following totals over 32 million, he has produced over 400 viral videos, which have amassed more than 5 billion views, and his podcast, \'On Purpose\', is consistently ranked the world\'s #1 health-related podcast.','image_path' => '../bookimage/think_like_a_monk.jpg','added_date' => '2020-06-30'),
+  array('book_id' => '7','bookname' => 'Olive, Again','author' => 'Elizabeth Strout','price' => '23.50','category' => 'English Books','quantity' => '46','detail' => 'Olive, Again is a 2019 novel by Elizabeth Strout, published by Random House on October 15, 2019. It is a sequel to Olive Kitteridge (2008). Similar to the first novel, Olive, Again takes the form of 13 short stories that are interrelated but discontinuous in terms of narrative. It follows Olive Kitteridge from her seventies into her eighties.
+','image_path' => '../bookimage/olive.jpg','added_date' => '2020-07-01'),
+  array('book_id' => '8','bookname' => 'Can\'t You Sleep, Little Bear?','author' => 'Martin Waddell','price' => '5.00','category' => 'Others','quantity' => '5','detail' => 'Celebrate 25 years of Big Bear, Little Bear and their cosy cave in this gorgeous anniversary edition of a classic bedtime favourite.
+Described as \'the most perfect children\'s book ever written\', this is a beautiful 25th anniversary edition of the Kate Greenaway-winning story of Little Bear, who just can\'t sleep. There is dark all around him in the Bear Cave. Not even Big Bear\'s biggest lantern can light up the darkness of the night outside. Can Big Bear find a way to reassure restless Little Bear and help him fall fast asleep?','image_path' => '../bookimage/bear.jpg','added_date' => '2020-07-01'),
+  array('book_id' => '9','bookname' => 'Let\'s Go Home, Little Bear','author' => 'Martin Waddell','price' => '4.00','category' => 'Others','quantity' => '63','detail' => 'Big Bear and Little Bear are returning home from a wonderful romp through the snowy woods, when a noise startles Little Bear. Plod, plod, plodÂ—suddenly the woods are alive with unseen Plodders and Drippers and Ploppers. Poor Little Bear is very scared. But Big Bear is beside him with comforting explanations and a piggyback ride to bring him safely home.','image_path' => '../bookimage/homebear.jpg','added_date' => '2020-07-01'),
+  array('book_id' => '10','bookname' => 'Bears, Bears, Bears ','author' => 'Martin Waddell','price' => '3.50','category' => 'Others','quantity' => '29','detail' => 'Ruby likes bears. So she puts up a sign inviting them to call. She isn\'t content with just one bear though - she wants more and more and more. But, as Ruby soon discovers, you can have too much of a good thing!
+
+Written by Martin Waddell, well known and respected the world over for his stunning storytelling. He wrote Farmer Duck and Can\'t You Sleep, Little Bear? both of which won the Smarties Prize, and the bestselling Owl Babies. Lee Wildish is the winner of the Red House Children\'s Book Award.','image_path' => '../bookimage/bearsssss.jpg','added_date' => '2020-07-01'),
+  array('book_id' => '11','bookname' => 'Kid Normal: Kid Normal','author' => 'Greg James','price' => '4.50','category' => 'Others','quantity' => '79','detail' => 'The first book in a laugh-out-loud funny adventure series for 8+ readers from radio stars Greg James and Chris Smith
+\'So funny, it\'s almost criminal\' INDEPENDENT
+\'Outrageous capers\' GUARDIAN
+SHORTLISTED FOR THE WATERSTONES CHILDREN\'S BOOK PRIZE
+A TOM FLETCHER BOOK CLUB PICK
+Murph Cooper has a problem.
+His new school is top secret, and super weird. His classmates can all fly or control the weather or conjure tiny horses from thin air. And what\'s Murph\'s extraordinary skill? Um, oh yeah â€“ he hasn\'t got one.
+Just as well there are no revolting supervillains lurking nearby, their minds abuzz with evil plans. There are!? Right. Ok, then...
+It\'s time for Kid Normal to become a hero!','image_path' => '../bookimage/kids.jpg','added_date' => '2020-07-01'),
+  array('book_id' => '12','bookname' => 'Slime: The new childrenâ€™s book from No. 1 bestselling author David Walliams.','author' => 'David Walliams','price' => '25.00','category' => 'Others','quantity' => '42','detail' => 'This little island is home to a large number of horrible grown-ups. The school, the local park, the toy shop and even the islandâ€™s ice-cream van are all run by awful adults who like nothing more than making children miserable. And the island is owned by the most awful one of all â€“ Aunt Greta Greed!
+Something needs to be done about them.
+But who could be brave enough?
+Meet Ned â€“ an extraordinary boy with a special power. SLIMEPOWER!','image_path' => '../bookimage/slime.jpg','added_date' => '2020-07-01'),
+  array('book_id' => '24','bookname' => 'æŠŠåæ—¥å­è¿‡æˆå¥½æ—¥å­ï¼šè§‚ç…§äº”ç§å†…åœ¨æœ¬è´¨ï¼Œæ‰¾å›žç”Ÿæ´»ä¸­çš„æ»¡è¶³æ„Ÿ','author' => 'ä½•æƒå³°','price' => '28.00','category' => 'Chinese Books','quantity' => '52','detail' => 'ä½ åŽŸæœ¬å°±æ˜¯å¿«ä¹çš„ï¼Œçƒ¦æ¼æ˜¯åŽæ¥æ‰æœ‰çš„ï¼Œå›žå½’å†…åœ¨æœ¬è´¨ï¼Œå°±æ˜¯å›žåˆ°æ²¡æœ‰çƒ¦æ¼ä»¥å‰çš„é‚£é¢—å¿ƒã€‚
+ä½•åŒ»å¸ˆæ•™ä½ è§‚ç…§äº”ç§å†…åœ¨çš„æœ¬è´¨ï¼Œæ‰¾å›žå®‰å¿ƒã€å¹³é™çš„è‡ªå·±ã€‚
+æˆ‘ä»¬æ¯å¤©è¿‡ç€ä¸€æ ·çš„æ—¥å­ï¼Œç”Ÿæ´»é‡Œçš„å†µå‘³åªæœ‰è‡ªå·±çŸ¥é“ã€‚
+ä½†æˆ‘ä»¬å´æ€»æ˜¯å‘å¤–æ¸´æ±‚â€œå¿«ä¹â€ï¼Œè¶Šæ‰¾è¶Šå¤±åŽ»è‡ªæˆ‘ï¼Œæƒ…ç»ªä¹Ÿè¢«åˆ«äººç‰µç€é¼»å­èµ°ã€‚
+çˆ±å› æ–¯å¦æ›¾è¯´ï¼šâ€œä¸€ç›´åšç€ç›¸åŒçš„äº‹å´å¸Œæœ›æœ‰æ‰€æ”¹å˜çš„äººæ˜¯ç–¯å­ã€‚â€
+è€Œæˆ‘ä»¬å´ä¸è‡ªè§‰åœ¨è¿™ä¸ªå¿«é€Ÿå˜è¿çš„ç¤¾ä¼šä¸­è¢«é€¼åˆ°ç²¾ç¥žç´§ç»·ã€‚
+
+ä½œè€…ç®€ä»‹:
+ä½•æƒå³°
+åŒ»å¸ˆå…¼ä½œå®¶ï¼ŒåŒæ—¶ä¹Ÿåœ¨å¤§å­¦æŽˆè¯¾ï¼Œä¸“é•¿æ˜¯è„‘ç¥žç»ç§‘å­¦ã€‚1995å¹´ï¼Œä»–å¼€å§‹å†™ä½œï¼Œæ—©æœŸä¸ºã€Šè”åˆæŠ¥ã€‹ã€ã€Šå¸¸æ˜¥æœˆåˆŠã€‹ã€ã€Šæ‹¾ç©—æ‚å¿—ã€‹ç­‰æ’°å†™åŒ»å­¦ä¸“æ ã€‚éšåŽï¼Œå› æŽ¥è§¦â€œå¿ƒç†ç¥žç»å…ç–«å­¦â€ï¼ˆPsychoneuroimmunologyï¼‰ï¼Œè§¦å‘äº†å¯¹â€œå¿ƒçµå±‚é¢â€çš„æŽ¢ç©¶ï¼ŒäºŽæ˜¯ä½•åŒ»å¸ˆæ”¹å˜äº†æ–¹å‘ï¼Œä»–å¼€å§‹å†™ä¸€äº›å¿ƒçµæˆé•¿å’ŒåŠ±å¿—ç±»çš„ä½œå“ã€‚ç›®å‰ï¼Œä»–æ‰€å‡ºç‰ˆçš„å¿ƒçµåŠ±å¿—ä¹¦ï¼Œå·²ç´¯ç§¯è¿‘60æœ¬ã€‚
+åœ¨å°æ¸¯æ¾³ã€æ–°é©¬ã€ä¸­å›½å¤§é™†ç­‰åœ°åŒºæ‹¥æœ‰ä¼—å¤šè¯»è€…ã€‚ä»–çš„ç¬”è§¦ç»†è…»ï¼Œå¹³æ˜“ä¸­æ˜¾å“²ç†ï¼Œå¦™è¶£ä¸­è§æ™ºæ…§ã€‚å¯¹é—®é¢˜æœ‰å¾ˆæ·±çš„æ´žæ‚‰ï¼Œä¸å•æ˜¯â€œè¯Šæ–­é—®é¢˜â€ï¼ŒåŒæ—¶è¿˜æä¾›è§†é‡Žå°†å¿ƒæå‡æ›´é«˜çš„å±‚æ¬¡ï¼Œâ€œå¦‚æ­¤ï¼Œè¯»è€…æ‰€èŽ·å¾—çš„ï¼Œå°±ä¸å•åªæ˜¯ä¹¦æœ¬ä¸Šçš„çŸ¥è¯†ï¼Œè€Œæ˜¯å…¨æ–°çš„äººç”Ÿã€‚â€ä½•åŒ»å¸ˆå¦‚æ˜¯è¯´ã€‚','image_path' => '../bookimage/goodlife.jpg','added_date' => '2020-07-20'),
+  array('book_id' => '17','bookname' => 'The Fault in Our Stars','author' => 'John Green','price' => '42.55','category' => 'English Books','quantity' => '28','detail' => 'The Fault in Our Stars is a novel by John Green. It is his fourth solo novel, and sixth novel overall. It was published on January 10, 2012. The title is inspired by Act 1, Scene 2 of Shakespeare\'s play Julius Caesar, in which the nobleman Cassius says to Brutus: "The fault, dear Brutus, is not in our stars, / But in ourselves, that we are underlings." The story is narrated by Hazel Grace Lancaster, a 16-year-old girl with thyroid cancer that has affected her lungs. Hazel is forced by her parents to attend a support group where she subsequently meets and falls in love with 17-year-old Augustus Waters, an ex-basketball player and amputee. An American feature film adaptation of the novel directed by Josh Boone and starring Shailene Woodley, Ansel Elgort, and Nat Wolff was released on June 6, 2014. A Hindi feature film adaptation is set to be released on 24 July 2020. Both the book and its American film adaptation were met with strong critical and commercial success.','image_path' => '../bookimage/The_Fault_in_Our_Stars.jpg','added_date' => '2020-07-01'),
+  array('book_id' => '18','bookname' => 'The Art of War','author' => 'Sun Tzu','price' => '12.00','category' => 'Chinese Books','quantity' => '14','detail' => 'The Art of War is an ancient Chinese military treatise dating from the Late Spring and Autumn Period (roughly 5th century BC). The work, which is attributed to the ancient Chinese military strategist Sun Tzu ("Master Sun", also spelled Sunzi), is composed of 13 chapters. Each one is devoted to an aspect of warfare and how it applies to military strategy and tactics. For almost 1,500 years it was the lead text in an anthology that would be formalised as the Seven Military Classics by Emperor Shenzong of Song in 1080. The Art of War remains the most influential strategy text in East Asian warfare[1] and has influenced both Eastern and Western military thinking, business tactics, legal strategy, lifestyles and beyond.','image_path' => '../bookimage/art0fwar.jpg','added_date' => '2020-07-01')
+);
+
+/* `libros`.`cart` */
+$cart = array(
+);
+
+/* `libros`.`cart_order` */
+$cart_order = array(
+  array('id' => '1','order_id' => '9','book_id' => '9','book_qty' => '3'),
+  array('id' => '2','order_id' => '10','book_id' => '18','book_qty' => '1'),
+  array('id' => '3','order_id' => '10','book_id' => '11','book_qty' => '1'),
+  array('id' => '4','order_id' => '11','book_id' => '12','book_qty' => '3'),
+  array('id' => '5','order_id' => '11','book_id' => '5','book_qty' => '1'),
+  array('id' => '6','order_id' => '12','book_id' => '17','book_qty' => '2'),
+  array('id' => '7','order_id' => '12','book_id' => '7','book_qty' => '2'),
+  array('id' => '8','order_id' => '18','book_id' => '4','book_qty' => '1'),
+  array('id' => '9','order_id' => '19','book_id' => '11','book_qty' => '1'),
+  array('id' => '10','order_id' => '20','book_id' => '10','book_qty' => '1'),
+  array('id' => '11','order_id' => '21','book_id' => '12','book_qty' => '1'),
+  array('id' => '12','order_id' => '21','book_id' => '8','book_qty' => '1'),
+  array('id' => '13','order_id' => '22','book_id' => '12','book_qty' => '1'),
+  array('id' => '14','order_id' => '22','book_id' => '4','book_qty' => '1')
+);
+
+/* `libros`.`comments` */
+$comments = array(
+  array('comment_id' => '3','username' => 'khorwei','book_id' => '10','topic' => 'Delivery On time','comment' => 'Order on 11 July received it the next day!','datetime' => '2020-07-17')
+);
+
+/* `libros`.`orders` */
+$orders = array(
+  array('order_id' => '11','user_id' => '1','order_qty' => '4','total_amount' => '115.00','orderdate' => '2020-07-04','status' => 'FAIL','ordernum' => '1291778869'),
+  array('order_id' => '12','user_id' => '1','order_qty' => '4','total_amount' => '132.10','orderdate' => '2020-07-04','status' => 'PROCESSING','ordernum' => '3910526460'),
+  array('order_id' => '22','user_id' => '1','order_qty' => '2','total_amount' => '45.00','orderdate' => '2020-07-21','status' => 'pending','ordernum' => '7463956440'),
+  array('order_id' => '21','user_id' => '1','order_qty' => '2','total_amount' => '30.00','orderdate' => '2020-07-21','status' => 'pending','ordernum' => '9646881117'),
+  array('order_id' => '20','user_id' => '1','order_qty' => '1','total_amount' => '3.50','orderdate' => '2020-07-21','status' => 'pending','ordernum' => '4258338553'),
+  array('order_id' => '19','user_id' => '1','order_qty' => '1','total_amount' => '4.50','orderdate' => '2020-07-21','status' => 'pending','ordernum' => '2122280899')
+);
+
+/* `libros`.`users` */
+$users = array(
+  array('user_id' => '1','username' => 'khorwei','user_password' => 'c33367701511b4f6020ec61ded352059','fullname' => 'Coway Yeap','phone' => '0123456789','email' => 'imcoway@gmail.com','address' => 'Tropicana Residence, Block A  08-03','city' => 'Bandar Sunway','postcode' => '47500','state' => 'Johor Bharu','country' => 'MALAYSIA'),
+  array('user_id' => '3','username' => 'user','user_password' => 'e10adc3949ba59abbe56e057f20f883e','fullname' => 'khorwei','phone' => '0123456789','email' => 'khorwei@gmail.com','address' => 'No 26, Jalan PJS 9/26','city' => 'Bandar Sunway','postcode' => '47500','state' => 'Selangor','country' => 'Malaysia')
+);
